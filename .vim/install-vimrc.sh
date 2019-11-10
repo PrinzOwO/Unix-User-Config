986146ce02cf89e1c496bd55d2a3a30e40c62076
張耀文 <hadeschang0322.cs04@g2.nctu.edu.tw>
2019-11-11 01:07:44 +0800

    Add YCM install script to auto install

--- .vim/install-vimrc.sh
+++ .vim/install-vimrc.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+vim_root_path=`pwd`
+
 cd ..
 ln -s .vim/vimrc .vimrc
 ln -s .vim/gvimrc .gvimrc
 
 # xmledit
-cd .vim/bundle/xmledit/ftplugin/
+cd $vim_root_path/bundle/xmledit/ftplugin/
 ln -s xml.vim html.vim
 ln -s xml.vim xhtml.vim
+
+# YouCompleteMe
+cd $vim_root_path/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py
+./install.py --clang-completer
+./install.py --gocode-completer
